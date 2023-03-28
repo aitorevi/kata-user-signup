faf3ad64ce875b1865052432c2b9237551585424
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-03-28 15:16:01 +0200

    Test(green): throws an error message if email not have the correct format

--- src/core/UserService.ts
+++ src/core/UserService.ts
@@ -5,8 +5,12 @@ export class UserService {
     }
 
     save(email: string) {
+
         if (email !== "") {
             this.userRepository.save(email)
+        }
+        if (email && !email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+            throw new Error(`${email} not have the email format`)
         }
     }
 
